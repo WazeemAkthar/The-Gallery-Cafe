68a92640c2b5a330c7a5a897beb57e7616edbfde
Wazeem Akthar <aktharwazeem@gmail.com>
2024-08-09 19:01:25 +0530

    updated

---
+++ Pages/MangeAOrders.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Orders</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="../CSS/Admin_Dashboard.css">
+</head>
+
+<body>
+    <div id="adminNav"></div>
+    <script src="../JS/components.js"></script>
+</body>
+
+</html>
